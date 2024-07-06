711538816bb67ce1d98162f1d12c1845e91dce97
apalah <155531891+axo-id@users.noreply.github.com>
2024-07-06 14:25:36 +0700

    Update config.js

--- data/js/config.js
+++ data/js/config.js
@@ -5,6 +5,6 @@ export const firebaseConfig = {
     projectId: "chats-app-12",
     storageBucket: "chats-app-12.appspot.com",
     messagingSenderId: "670073879651",
-    appId: "1:670073879651:web:dc134fb1dde15710478c56",
-    measurementId: "G-C88E1KS3XY"
+    appId: "1:670073879651:web:2069d5934d1c20bd478c56",
+    measurementId: "G-W9MEPB5GC4"
 };
